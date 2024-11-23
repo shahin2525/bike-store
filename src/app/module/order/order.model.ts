83a37b712b7f4cb52d3d7597a6d260b6b8bd46de
shahin2525 <shahin451999@gmail.com>
2024-11-22 22:47:38 -0800

    add order

---
+++ src/app/module/order/order.model.ts
@@ -0,0 +1,20 @@
+import { model, Schema } from 'mongoose';
+import { TOrder } from './order.interface';
+
+const OrderSchema = new Schema<TOrder>({
+  email: { type: String, required: [true, 'email is required'] },
+  product: {
+    type: Schema.Types.ObjectId,
+    ref: 'Bike',
+    required: [true, 'bike is required'],
+  },
+  quantity: { type: Number, required: [true, 'quantity is required'], min: 1 },
+  totalPrice: {
+    type: Number,
+    required: [true, 'total price is required'],
+    min: 0,
+  },
+});
+
+// 3. Create a Model.
+export const Order = model<TOrder>('Order', OrderSchema);
