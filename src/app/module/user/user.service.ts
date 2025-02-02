220b90ecd17f2fd709a82e163a0fab700876bb5e
shahin2525 <shahin451999@gmail.com>
2025-02-01 23:09:40 -0800

    add cheaking exists user

--- src/app/module/user/user.service.ts
+++ src/app/module/user/user.service.ts
@@ -2,6 +2,10 @@ import { TUser } from './user.interface';
 import { User } from './user.model';
 
 const createUserIntoDB = async (payload: TUser) => {
+  // const user = await User.isUserExists(payload.email)
+  if (await User.isUserExists(payload.email)) {
+    throw new Error('user is already exists');
+  }
   const result = await User.create(payload);
   return result;
 };
