cf88a05d6ea4df2d0594265a617353d890f41244
shahin2525 <shahin451999@gmail.com>
2025-02-18 17:58:44 -0800

    fix sp_return_url

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://bike-store-ashy.vercel.app'],
+    origin: ['https://bike-shop-frontend.vercel.app'],
     credentials: true,
   }),
 );
