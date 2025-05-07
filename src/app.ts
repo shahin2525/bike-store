9538b8fc728c3f48d786c587393417db20a118ad
shahin2525 <shahin451999@gmail.com>
2025-05-07 12:09:16 +0530

    add localhost origin

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['https://bike-shop-frontend.vercel.app'],
+    origin: ['https://bike-shop-frontend.vercel.app', 'http://localhost:5173'],
     credentials: true,
   }),
 );
